dad31987da2aceb3ce8506fcc6a967cec55f3d63
tmali16 <don.kg16@gmail.com>
2018-05-19 15:13:51 +0600

    rabota

--- Posts/activate.py
+++ Posts/activate.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from Posts.models import Post
+from datetime import timedelta
+import time
 import threading
 
 
@@ -12,6 +14,7 @@ def checks():
                 a_active = get_object_or_404(Post, id=id).admin_active
                 u_active = get_object_or_404(Post, id=id).user_active
                 e_date = get_object_or_404(Post, id=id).end_date
+                s_date = get_object_or_404(Post, id=id).start_date
                 if a_active and u_active:
                     if timezone.now() > e_date:
                         Post.objects.filter(id=id).update(admin_active=False)
@@ -19,6 +22,19 @@ def checks():
                     else:
                         Post.objects.filter(id=id).update(start_date=timezone.now())
                         print('Anketa #'+str(id)+' works')
+                else:
+                    if not u_active and a_active:
+                        res_date = timezone.now() - e_date
 
-                    # Post.objects.values('id', 'user_active', 'admin_active', 'start_date', 'end_date').filter(
-                    # user_active=True).filter(admin_active=True):
+def count_to_end_active(active_date, id):
+    today = timezone.now()
+    active_to_date = timedelta(days=active_date)
+    end_date = get_object_or_404(Post, id=id).end_date
+    while True:
+        if timezone.now() > end_date:
+            Post.objects.filter(id=id).update(admin_active=False)
+            print('deActivated')
+            break
+        else:
+            return (timezone.now()-end_date)
+            time.sleep(86400)
